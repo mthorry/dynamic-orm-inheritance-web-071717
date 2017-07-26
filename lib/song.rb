3f14e0e30ef50a8af6c6c51d3f600b2e7ee90ff6
mthorry <mthorry@gmail.com>
2017-07-26 17:31:26 -0400

    done

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,6 @@
 require_relative "./interactive_record.rb"
 
-class Song < InteractiveRecord
+class Song < InteractiveRecord # inherits ALL code from InteravtiveRecord, which is totally abstracted. Just need to create our accessors
 
   self.column_names.each do |col_name|
     attr_accessor col_name.to_sym
